9ceb8c7db9294bc1b685ae38e5bee71825e92328
Juan Soares <wjuan.ps@gmail.com>
2022-03-31 13:38:48 -0300

    create routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Roles\Admin\CompanyController;
+use App\Http\Controllers\Roles\Admin\EmployeeController;
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Roles\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Roles\Company\HomeController as CompanyHomeController;
+use App\Http\Controllers\Roles\Employee\HomeController as EmployeeHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +21,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [ LoginController::class, 'showLoginForm' ])->name('login');
+Route::post('/logout', [ LoginController::class, 'logout' ])->name('logout');
+Route::post('/login', [ LoginController::class, 'login' ]);
+
+Route::get('/', [ HomeController::class, 'index' ])->name('home');
+Route::get('/home', [ HomeController::class, 'index' ])->name('home');
+
+Route::middleware('UserAdmin')->prefix('admin')->group(function() {
+  Route::get('/', [ AdminHomeController::class, 'index' ])->name('user_admin.home');
+
+  Route::prefix('/companies')->group(function () {
+    Route::get('/', [ CompanyController::class, 'index' ])->name('user_admin.company.index');
+    Route::get('/create', [ CompanyController::class, 'showCreateCompanyForm' ])->name('user_admin.company.new');
+    Route::post('/create', [ CompanyController::class, 'create' ])->name('user_admin.company.create');
+    Route::get('/{company_id}', [ CompanyController::class, 'show' ])->name('user_admin.company.show');
+    Route::get('/{company_id}/edit', [ CompanyController::class, 'showUpdateCompanyForm' ])->name('user_admin.company.edit');
+    Route::put('/{company_id}/edit', [ CompanyController::class, 'handleEdit' ])->name('user_admin.company.handle.edit');
+    Route::delete('/{company_id}/delete', [ CompanyController::class, 'handleDelete' ])->name('user_admin.company.handle.delete');
+
+    Route::prefix('{company_id}/employees')->group(function() {
+      Route::get('/create', [ EmployeeController::class, 'showCreateEmployeeForm' ])->name('user_admin.employee.new');
+      Route::post('/create', [ EmployeeController::class, 'create'])->name('user_admin.employee.create');
+      Route::get('/{employee_id}', [ EmployeeController::class, 'showUpdateEmployeeForm' ])->name('user_admin.employee.show');
+      Route::put('/{employee_id}/edit', [ EmployeeController::class, 'handleEdit' ])->name('user_admin.employee.handle.edit');
+      Route::delete('/{employee_id}/delete', [ EmployeeController::class, 'handleDelete' ])->name('user_admin.employee.handle.delete');
+    });
+  });
+
+  Route::get('/employees', [ EmployeeController::class, 'index' ])->name('user_admin.employee.index');
 });
 
-Auth::routes();
+Route::middleware('UserCompany')->prefix('company')->group(function () {
+  Route::get('/', [ CompanyHomeController::class, 'index' ])->name('user_company.home');
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('UserEmployee')->prefix('employee')->group(function () {
+  Route::get('/', [ EmployeeHomeController::class, 'index' ])->name('user_employee.home');
+});
